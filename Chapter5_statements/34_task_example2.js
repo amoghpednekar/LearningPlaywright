877f0d34475ffb92cc19fa53aefb39e88166a0d9
amoghpednekar <amoghpednekar616@gmail.com>
2026-02-21 11:20:08 +0530

    Update 34_task_example2.js

--- Chapter5_statements/34_task_example2.js
+++ Chapter5_statements/34_task_example2.js
@@ -5,18 +5,12 @@
 for (let myNumber = 1; myNumber <= 100; myNumber++) {
     if (myNumber % 3 === 0) {
         console.log("Fizz");
-
     } else if (myNumber % 5 === 0) {
         console.log("Buzz");
-
     } else if (myNumber % 3 === 0 && myNumber % 5 === 0) {
         console.log("FizzBuzz");
-
     }
     else {
         console.log(myNumber);
-
-
     }
-
 }
