0f66c2ba101730e848efe782cd24deddf2ff5481
amoghpednekar <amoghpednekar616@gmail.com>
2026-03-03 15:21:43 +0530

    Add 55_while_loop_task.js

---
+++ Chatper7_loops/55_while_loop_task.js
@@ -0,0 +1,44 @@
+// Task 1: Print "Playwright" 5 times
+let i = 0;
+while (i < 5) {
+    console.log("Playwright");
+    i++;
+}
+
+//Task 2: Print numbers from 1 to 10
+
+let numbers = 1;
+while (numbers <= 10) {
+    console.log(numbers);
+    numbers++;
+}
+
+//Task 3: Print even numbers from 1 to 20
+
+let numb = 2;
+while (numb <= 20) {
+    console.log(numb);
+    numb += 2; // need this steps for increment as per conditions
+}
+
+// Task 4: Calculate sum of first 10 natural numbers
+// Natural number start from 1 to 10
+
+let num = 1;
+let sum = 0;
+while (num <= 10) {
+    sum = sum + num;
+    num = num + 1;
+}
+console.log(sum);
+
+// Task 5: Print the multiplication table of 7
+
+let numbr = 7;
+let multiplyNum = 1;
+let multiply;
+while (multiplyNum <= 10) {
+    multiply = numbr * multiplyNum;
+    multiplyNum = multiplyNum + 1;
+    console.log(multiply);
+}
