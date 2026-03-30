5e86d532568bbc99a0a5b5f8dfafc37e1cb7e8cd
amoghpednekar <amoghpednekar616@gmail.com>
2026-03-30 20:23:03 +0530

    Add string conversions examples

---
+++ Chapter10_Strings/103_string_conversions.js
@@ -0,0 +1,30 @@
+// String Conversion:
+let number = 200;
+let newnumber = number.toString();
+console.log(typeof (number));
+console.log(typeof (newnumber));
+
+let booleain = true;
+let newboolean = booleain.toString();
+console.log(typeof (booleain));
+console.log(typeof (newboolean));
+
+//Number("42");
+console.log(Number("42")); // 42 will be output
+
+//parseInt:
+console.log(parseInt("45px"));
+//parseFloat:
+console.log(parseFloat("3.23cm"));
+
+let str = "Hellow"
+console.log(str[0]); //H
+str[1] = "E";
+console.log(str); // cant' update the value
+
+let upper = str.toUpperCase();//store it into new variable to check updates
+console.log(upper);
+
+
+
+
